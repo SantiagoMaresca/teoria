94b078fbd4fcd8cb93b1d97808c25a2f34ad2f46
SantiagoMaresca <santiago.maresca26@gmail.com>
2019-11-08 18:50:13 -0300

    Create env
    -

---
+++ proyecto.py
@@ -0,0 +1,7 @@
+def searchKDTree(kdTree, point, dim = 0):
+    if len(kdTree) == 1:
+        return kdTree[0] == point
+    nodeValue, left, right = kdTree
+    nextDim = (dim + 1) % len(point)
+    nextTree = left if point[dim] < nodeValue else right
+    return searchKDTree(nextTree, point, nextDim)
