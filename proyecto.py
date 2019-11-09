241e7fd64ce97eb31eb9fac1d42f76bcb5d0a928
Lucas Lois <lucasloisp@gmail.com>
2019-11-09 10:12:53 -0300

    Trabajo del Viernes 08/11. En proceso.

--- proyecto.py
+++ proyecto.py
@@ -5,3 +5,39 @@ def searchKDTree(kdTree, point, dim = 0):
     nextDim = (dim + 1) % len(point)
     nextTree = left if point[dim] < nodeValue else right
     return searchKDTree(nextTree, point, nextDim)
+
+def makeKDTree(points, dim = 0):
+    if not points:
+        return None # Empty trees are None
+    elif len(points) == 1:
+        return tuple(points) # Leaf nodes have one point.
+    points.sort(key = lambda p: p[dim])
+    medianIndex = len(points) // 2
+    median = points[medianIndex][dim]
+    nextDim = (dim + 1) % len(points[medianIndex])
+    left = [p for p in points if p[dim] < median]
+    right = [p for p in points if p[dim] >= median]
+    return (median, makeKDTree(left, nextDim), makeKDTree(right, nextDim))
+
+def getMedians(points, r, dim):
+    """Se obtienen las r medianas desde dim"""
+    return [points[0][i] for i in range(r)]
+
+def matches_criteria()
+
+def makeKDRTree(points, r, dim = 0):
+    if not points:
+        return None # Empty trees are None
+    elif len(points) == 1:
+        return tuple(points) # Leaf nodes have one point.
+
+    medians = getMedians(points, r, dim)
+    partitions = [[p for p in points if p[dim] < median] for criteria in itertools.product([False, True], repeat=r)]
+
+    points.sort(key = lambda p: p[dim])
+    medianIndex = len(points) // 2
+    median = points[medianIndex][dim]
+    nextDim = (dim + 1) % len(points[medianIndex])
+    left = [p for p in points if p[dim] < median]
+    right = [p for p in points if p[dim] >= median]
+    return (median, makeKDTree(left, nextDim), makeKDTree(right, nextDim))
