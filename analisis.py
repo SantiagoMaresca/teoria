8b8fde6cca006e6877c34bcd6c501c18317aadf0
Lucas Lois <lucasloisp@gmail.com>
2019-11-21 19:30:31 -0300

    MAs graficas

--- analisis.py
+++ analisis.py
@@ -22,7 +22,6 @@ if __name__ == '__main__':
     df_crear_kdr = df[df["tipo"] == CREAR_KDR]
     df_crear_kd = df[df["tipo"] == CREAR_KD]
 
-
     fig = plt.figure(figsize=(10,10))
     # Tiempo buscar positivo vs k
     for i, r in enumerate(range(1, 5+1)):
@@ -66,4 +65,91 @@ if __name__ == '__main__':
         fig.legend(handles, labels, loc=(0.7, 0.2))
     fig.tight_layout()
     plt.savefig(f'informe/img/buscar_neg_n.png')
+
+    fig = plt.figure(figsize=(10,10))
+    # Tiempo buscar positivo vs k
+    for i, r in enumerate(range(1, 5+1)):
+        ax = fig.add_subplot(f'32{i+1}')
+        (
+            df_buscar_pos_kdr[df_buscar_pos_kdr['r'] == r]
+            .groupby(['k', 'n'], as_index=False)['tiempo']
+            .mean()
+            .pivot(index='n', columns='k', values='tiempo')
+            .plot(style='.-', ax=ax)
+        )
+        (
+            df_buscar_neg_kdr[df_buscar_neg_kdr['r'] == r]
+            .groupby(['k', 'n'], as_index=False)['tiempo']
+            .mean()
+            .pivot(index='n', columns='k', values='tiempo')
+            .plot(style='x-', ax=ax)
+        )
+        ax.set_xlim([10**5 - 10**4, 10**6 + 10**4])
+        ax.set_ylabel('tiempo (ms)')
+        ax.set_xticks([10**5, 5 * 10**5, 10**6])
+        ax.margins(.05)
+        ax.title.set_text(f'r={r}')
+        ax.get_legend().remove()
+        handles, labels = ax.get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.7, 0.2))
+    fig.tight_layout()
+    plt.savefig(f'informe/img/buscar_n.png')
+
+    fig = plt.figure(figsize=(10,10))
+    # Tiempo buscar positivo vs k
+    for i, r in enumerate(range(1, 5+1)):
+        ax = fig.add_subplot(f'32{i+1}')
+        (
+            df_buscar_pos_kdr[df_buscar_pos_kdr['r'] == r]
+            .groupby(['k', 'n'], as_index=False)['tiempo']
+            .mean()
+            .pivot(index='n', columns='k', values='tiempo')
+            .plot(style='.-', ax=ax)
+        )
+        (
+            df_buscar_pos_kd
+            .groupby(['k', 'n'], as_index=False)['tiempo']
+            .mean()
+            .pivot(index='n', columns='k', values='tiempo')
+            .plot(style='x-', ax=ax)
+        )
+        ax.set_xlim([10**5 - 10**4, 10**6 + 10**4])
+        ax.set_ylabel('tiempo (ms)')
+        ax.set_xticks([10**5, 5 * 10**5, 10**6])
+        ax.margins(.05)
+        ax.title.set_text(f'r={r}')
+        ax.get_legend().remove()
+        handles, labels = ax.get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.7, 0.1))
+    fig.tight_layout()
+    plt.savefig(f'informe/img/buscar_pos_kd_n.png')
+
+    fig = plt.figure(figsize=(10,10))
+    # Tiempo buscar positivo vs k
+    for i, r in enumerate(range(1, 5+1)):
+        ax = fig.add_subplot(f'32{i+1}')
+        (
+            df_buscar_neg_kdr[df_buscar_neg_kdr['r'] == r]
+            .groupby(['k', 'n'], as_index=False)['tiempo']
+            .mean()
+            .pivot(index='n', columns='k', values='tiempo')
+            .plot(style='.-', ax=ax)
+        )
+        (
+            df_buscar_neg_kd
+            .groupby(['k', 'n'], as_index=False)['tiempo']
+            .mean()
+            .pivot(index='n', columns='k', values='tiempo')
+            .plot(style='x-', ax=ax)
+        )
+        ax.set_xlim([10**5 - 10**4, 10**6 + 10**4])
+        ax.set_ylabel('tiempo (ms)')
+        ax.set_xticks([10**5, 5 * 10**5, 10**6])
+        ax.margins(.05)
+        ax.title.set_text(f'r={r}')
+        ax.get_legend().remove()
+        handles, labels = ax.get_legend_handles_labels()
+        fig.legend(handles, labels, loc=(0.7, 0.1))
+    fig.tight_layout()
+    plt.savefig(f'informe/img/buscar_neg_kd_n.png')
 
